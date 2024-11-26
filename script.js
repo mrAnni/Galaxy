40103dc9999f12a82a855afecab80f2f4c39d2dd
mrAnni <142735698+mrAnni@users.noreply.github.com>
2024-11-26 16:59:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// function createStars() {
+//   const container = document.querySelector("body");
+//   for (let i = 0; i < 1000; i++) {
+//     // Increase the number of stars to 1000
+//     const star = document.createElement("div");
+//     star.className = "star";
+//     star.style.width = ".1px";
+//     star.style.height = ".1px";
+//     star.style.top = Math.random() * 100 + "%";
+//     star.style.left = Math.random() * 100 + "%";
+//     container.appendChild(star);
+//   }
+// }
+// createStars();
+// let color ="Green";
+// switch(color){
+//   case "Red" :
+//     console.log("Stop");
+//     break;
+//     case "Yellow":
+//       console.log("Go slow");
+//     break;
+//   case "Green":
+//     console.log("You can go");
+//     default :
+//     console.log("broken light");
+//   }
+ 
+
+let ground ="Wankhede";
+switch(ground){
+  case "chinnaswami":
+  console.log("it is in Bangluru");
+  break;
+  case "Wankhede" :
+  console.log("It is in Mumbai");
+  break;
+  case "Edern gardern" :
+  console.log("It is in Kolkatta");
+
+}
+
+
